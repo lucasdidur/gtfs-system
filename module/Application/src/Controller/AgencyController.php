d0c9addbbd4dbfa970c5e3231c5f3c82ba5ef039
Lucas Didur <lucasdidur@hotmail.com>
2017-04-23 19:29:27 -0300

    first commit

---
+++ module/Application/src/Controller/AgencyController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AgencyController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
---
+++ start.bat
@@ -0,0 +1,3 @@
+start "" http://localhost:8080
+composer run serve --timeout=0
+pause
